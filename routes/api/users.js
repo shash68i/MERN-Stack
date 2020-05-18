00c6320b12dd8a49ed48376cf8ad1f9882a8d415
shashank <theshashank6870@gmail.com>
2020-05-18 23:43:25 +0530

    User registration is done and JTW is sent back as response

--- routes/api/users.js
+++ routes/api/users.js
@@ -58,7 +58,22 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save(); // Now user is saved
 
-
+      // Return Json Web Token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
