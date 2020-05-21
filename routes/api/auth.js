4bb2addafe90ba6e7b3991275e99b8f210721d87
shashank <theshashank6870@gmail.com>
2020-05-21 21:52:21 +0530

    Profile Schema created

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
+const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+
 
 // @route   GET api/auth
 // @desc    Test route
